0bb409548aa43e06fa335f16d8a7bbfd86ed03cf
jzyustc <jzy_ustc@mail.ustc.edu.cn>
2021-05-19 19:51:30 +0800

    fix for img2ply

--- planercnn/bg2ply.py
+++ planercnn/bg2ply.py
@@ -1,10 +1,15 @@
 from utils.get3d import *
 
 
-args = parse_args()
+def get3d(args):
+	evaluate(args)
 
-args.keyname = args.dataset
-args.test_dir = 'results/3d_models/'
 
-# python bg2ply.py --methods=f --suffix=warping_refine --dataset=inference --customDataFolder=plancercnn/example_images
-evaluate(args)
+if __name__ == '__main__':
+	args = parse_args()
+
+	args.keyname = args.dataset
+	args.test_dir = 'results/3d_models/'
+
+	# python bg2ply.py --methods=f --suffix=warping_refine --dataset=inference --customDataFolder=plancercnn/example_images
+	get3d(args)
--- pre_bg2ply.py
+++ pre_bg2ply.py
@@ -1,4 +1,4 @@
-from planercnn.utils.get3d import *
+from planercnn.bg2ply import get3d
 
 '''
 STEP 2.0 : Reconstruct a 3D model from a single rgb image
@@ -7,9 +7,13 @@ STEP 2.0 : Reconstruct a 3D model from a single rgb image
 if __name__ == "__main__":
 	args = parse_args()
 
-	args.keyname = args.dataset
 	results_path = "results/"  # change it
+
+	args.methods = 'f'
+	args.suffix = 'warping_refine'
+	args.dataset = 'inference'
+	args.customDataFolder = 'plancercnn/example_images'
+	args.keyname = args.dataset
 	args.test_dir = results_path + '3d_models/'
 
-	# python pre_bg2ply.py --methods=f --suffix=warping_refine --dataset=inference --customDataFolder=plancercnn/example_images
-	evaluate(args)
+	get3d(args)
